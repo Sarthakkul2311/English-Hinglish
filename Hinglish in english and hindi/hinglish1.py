3b0ca781c8b194864acb6920d715634a7ca21a13
Sarthak Kulkarni <80700249+Sarthakkul2311@users.noreply.github.com>
2023-09-19 23:40:12 +0530

    Update hinglish1.py

--- Hinglish in english and hindi/hinglish1.py
+++ Hinglish in english and hindi/hinglish1.py
@@ -1,4 +1,4 @@
-from googletrans import Translator, LANGUAGES
+from googletrans import Translator
 
 def translate_to_hinglish(text):
     # Create a Translator object
@@ -14,20 +14,11 @@ def translate_to_hinglish(text):
     # Translate the English text to Hindi
     hindi_translation = translator.translate(text, src='en', dest='hi')
 
-    # Replace common English words with their Hinglish equivalents
+    # Replace specific Hindi words with their English equivalents
     hinglish_translation = hindi_translation.text
-    hinglish_translation = hinglish_translation.replace('feedback', 'feedback')
-    hinglish_translation = hinglish_translation.replace('comment section', 'comment section')
-    hinglish_translation = hinglish_translation.replace('products', 'products')
-    hinglish_translation = hinglish_translation.replace('clearly', 'clearly')
-    hinglish_translation = hinglish_translation.replace('mention', 'mention')
-    hinglish_translation = hinglish_translation.replace('waiting', 'waiting')
-    hinglish_translation = hinglish_translation.replace('bag', 'bag')
-
-    # Replace 'video' with the same word in English
+    hinglish_translation = hinglish_translation.replace('वीडियो', 'video')
     hinglish_translation = hinglish_translation.replace('टिप्पणी अनुभाग', 'comment section')
     hinglish_translation = hinglish_translation.replace('प्रतिक्रिया', 'feedback')
-    hinglish_translation = hinglish_translation.replace('वीडियो', 'video')
     hinglish_translation = hinglish_translation.replace('उत्पादों', 'products')
     hinglish_translation = hinglish_translation.replace('उल्लेख', 'mention')
     hinglish_translation = hinglish_translation.replace('बैग', 'bag')
